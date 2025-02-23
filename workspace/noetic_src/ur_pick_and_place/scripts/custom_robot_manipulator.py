39ce66a33e286f9875ecfa0f368cfa301af08851
FariqJavier <fariqjavier@gmail.com>
2025-02-23 15:40:55 +0700

    chore: refactor robot_manipulator for arm controller

--- workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_manipulator.py
+++ workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_manipulator.py
@@ -7,19 +7,17 @@ from trac_ik_python.trac_ik import IK
 from geometry_msgs.msg import PoseStamped
 from trajectory_msgs.msg import JointTrajectory, JointTrajectoryPoint
 
-class InverseKinematics:
+class RobotManipulator:
     def __init__(self):
         rospy.init_node('custom_robot_manipulator')
         self.robot = moveit_commander.RobotCommander()
         self.arm_group = moveit_commander.MoveGroupCommander("arm")
         
-        self.pose_sub = rospy.Subscriber("/robot_pose", PoseStamped, self.pose_callback)
-        self.traj_pub = rospy.Publisher("/arm_joint_trajectory", JointTrajectory, queue_size=10)
+        self.arm_pose_sub = rospy.Subscriber("/robot_pose", PoseStamped, self.arm_pose_callback)
+        self.arm_traj_pub = rospy.Publisher("/arm_joint_trajectory", JointTrajectory, queue_size=10)
         
-        # self.load_configuration()
-        
-        self.ik_solver = IK("base_link", "flange")
-        self.joint_names = [ 
+        self.arm_ik_solver = IK("base_link", "flange")
+        self.arm_joint_names = [ 
             "shoulder_pan_joint",
             "shoulder_lift_joint",
             "elbow_joint",
@@ -28,58 +26,50 @@ class InverseKinematics:
             "wrist_3_joint"
         ]
         
-    # def load_configuration(self):
-    #     with open('~/catkin_ws/src/ur_pick_and_place/config/ur5/joint_limits.yaml', 'r') as file:
-    #         self.joint_limits = yaml.safe_load(file)
-    #     with open('~/catkin_ws/src/ur_pick_and_place/config/ur5//physical_parameters.yaml', 'r') as file:
-    #         self.physical_params = yaml.safe_load(file)
-    #     with open('~/catkin_ws/src/ur_pick_and_place/config/ur5/calibration.yaml', 'r') as file:
-    #         self.kinematics = yaml.safe_load(file)
-    #     with open('~/catkin_ws/src/ur_pick_and_place/config/ur5/controllers.yaml', 'r') as file:
-    #         self.controllers = yaml.safe_load(file)
-        
-    def pose_callback(self, msg):
-        current_pose = msg.pose
-        rospy.loginfo("Received end-effector position: %s", current_pose)
+    def arm_pose_callback(self, msg):
+        current_arm_pose = msg.pose
+        rospy.loginfo("Received end-effector position: %s", current_arm_pose)
 
         # Set a new target position (e.g., move slightly upwards)
-        target_pose = PoseStamped()
-        target_pose.header.stamp = rospy.Time.now()
-        target_pose.header.frame_id = "base_link"
-        target_pose.pose.position.x = current_pose.position.x
-        target_pose.pose.position.y = current_pose.position.y
-        target_pose.pose.position.z = current_pose.position.z + 0.2  # Move 20cm up
-        target_pose.pose.orientation = current_pose.orientation  # Keep the same orientation
+        target_arm_pose = PoseStamped()
+        target_arm_pose.header.stamp = rospy.Time.now()
+        target_arm_pose.header.frame_id = "base_link"
+
+        target_arm_pose.pose.position.x = current_arm_pose.position.x
+        target_arm_pose.pose.position.y = current_arm_pose.position.y
+        target_arm_pose.pose.position.z = current_arm_pose.position.z + 0.2  # Move 20cm up
+        target_arm_pose.pose.orientation = current_arm_pose.orientation  # Keep the same orientation
 
         # Compute inverse kinematics to get joint positions
-        sol = self.ik_solver.get_ik(
-            [0.0] * len(self.joint_names),  # Initial joint seed
-            target_pose.pose.position.x, target_pose.pose.position.y, target_pose.pose.position.z,
-            target_pose.pose.orientation.x, target_pose.pose.orientation.y, target_pose.pose.orientation.z, target_pose.pose.orientation.w
+        sol = self.arm_ik_solver.get_ik(
+            [0.0] * len(self.arm_joint_names),  # Initial joint seed
+            target_arm_pose.pose.position.x, target_arm_pose.pose.position.y, target_arm_pose.pose.position.z,
+            target_arm_pose.pose.orientation.x, target_arm_pose.pose.orientation.y, target_arm_pose.pose.orientation.z, target_arm_pose.pose.orientation.w
         )
         
         if sol:
-            self.publish_trajectory(sol)
+            self.publish_arm_trajectory(sol)
         else:
             rospy.logwarn("IK solution not found")
     
-    def publish_trajectory(self, joint_positions):
+    def publish_arm_trajectory(self, arm_joint_positions):
         traj_msg = JointTrajectory()
-        traj_msg.joint_names = self.joint_names
+        # traj_msg.arm_joint_names = self.arm_joint_names
         
         point = JointTrajectoryPoint()
-        point.positions = joint_positions
+        point.positions = arm_joint_positions
         point.time_from_start = rospy.Duration(1.0)
         
         traj_msg.points.append(point)
         traj_msg.header.stamp = rospy.Time.now()
-        self.traj_pub.publish(traj_msg)
+        traj_msg.header.frame_id = "base_link"
+        self.arm_traj_pub.publish(traj_msg)
         rospy.loginfo("Published joint trajectory: %s", traj_msg)
 
 if __name__ == '__main__':
     moveit_commander.roscpp_initialize([])
     try:
-        node = InverseKinematics()
+        robotManipulator = RobotManipulator()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
