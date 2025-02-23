9707e5961759299f51e56929fe21b1f3bd846cdb
FariqJavier <fariqjavier@gmail.com>
2025-02-23 15:40:14 +0700

    chore: refactor to class robot_pose

--- workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_pose.py
+++ workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_pose.py
@@ -3,57 +3,51 @@ import rospy
 import tf
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import quaternion_from_euler
+class RobotPose:
+    def __init__(self):
+        rospy.init_node('custom_robot_pose', anonymous=True)
+        self.listener = tf.TransformListener()
+        self.pose_pub = rospy.Publisher("/robot_pose", PoseStamped, queue_size=10)
+        self.rate = rospy.Rate(10)
 
-def get_end_effector_position(listener):
+    def get_end_effector_position(self):
+        try:
+            self.listener.waitForTransform('/base_link', '/flange', rospy.Time(0), rospy.Duration(4.0))
+            self.listener.waitForTransform('/flange', '/robotiq_arg2f_base_link', rospy.Time(0), rospy.Duration(4.0))
 
-    # Wait for the transforms to be available
-    listener.waitForTransform('/base_link', '/flange', rospy.Time(0), rospy.Duration(4.0))
-    listener.waitForTransform('/flange', '/robotiq_arg2f_base_link', rospy.Time(0), rospy.Duration(4.0))
+            (trans_base_to_flange, quat_base_to_flange) = self.listener.lookupTransform('/base_link', '/flange', rospy.Time(0))
+            (trans_flange_to_gripper, quat_flange_to_gripper) = self.listener.lookupTransform('/flange', '/robotiq_arg2f_base_link', rospy.Time(0))
 
-    try:
-        # Get the transform from /base_link to /flange
-        (trans_base_to_flange, quat_base_to_flange) = listener.lookupTransform('/base_link', '/flange', rospy.Time(0))
+            end_effector_position = PoseStamped()
+            end_effector_position.header.stamp = rospy.Time.now()
+            end_effector_position.header.frame_id = "base_link"
 
-        # Get the transform from /flange to /robotiq_arg2f_base_link (gripper)
-        (trans_flange_to_gripper, quat_flange_to_gripper) = listener.lookupTransform('/flange', '/robotiq_arg2f_base_link', rospy.Time(0))
+            end_effector_position.pose.position.x = trans_base_to_flange[0] + trans_flange_to_gripper[0]
+            end_effector_position.pose.position.y = trans_base_to_flange[1] + trans_flange_to_gripper[1]
+            end_effector_position.pose.position.z = trans_base_to_flange[2] + trans_flange_to_gripper[2]
 
-        # Combine the transformations to get the gripper's position relative to /base_link
-        end_effector_position = PoseStamped()
-        end_effector_position.header.stamp = rospy.Time.now()
-        end_effector_position.header.frame_id = "base_link"  # Reference frame
+            quat_combined = tf.transformations.quaternion_multiply(quat_base_to_flange, quat_flange_to_gripper)
 
-        # Add translation
-        end_effector_position.pose.position.x = trans_base_to_flange[0] + trans_flange_to_gripper[0]
-        end_effector_position.pose.position.y = trans_base_to_flange[1] + trans_flange_to_gripper[1]
-        end_effector_position.pose.position.z = trans_base_to_flange[2] + trans_flange_to_gripper[2]
+            end_effector_position.pose.orientation.x = quat_combined[0]
+            end_effector_position.pose.orientation.y = quat_combined[1]
+            end_effector_position.pose.orientation.z = quat_combined[2]
+            end_effector_position.pose.orientation.w = quat_combined[3]
 
-        # Multiply quaternions (base_to_flange * flange_to_gripper) to get the final orientation
-        quat_combined = tf.transformations.quaternion_multiply(quat_base_to_flange, quat_flange_to_gripper)
+            return end_effector_position
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+            rospy.logerr("Transform not available!")
+            return None
 
-        # Set the orientation of the end-effector
-        end_effector_position.pose.orientation.x = quat_combined[0]
-        end_effector_position.pose.orientation.y = quat_combined[1]
-        end_effector_position.pose.orientation.z = quat_combined[2]
-        end_effector_position.pose.orientation.w = quat_combined[3]
-
-        return end_effector_position
-    except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
-        rospy.logerr("Transform not available!")
-        return None
+    def publish_pose(self):
+        while not rospy.is_shutdown():
+            pose = self.get_end_effector_position()
+            if pose:
+                self.pose_pub.publish(pose)
+                rospy.loginfo("Published End-effector Pose to /robot_pose: %s", pose)
+            self.rate.sleep()
 
 if __name__ == "__main__":
     print("RobotPose script started")
 
-    # Initialize the node
-    rospy.init_node('custom_robot_pose', anonymous=True)
-    # Create a TF listener
-    listener = tf.TransformListener()
-    # Initialize the publisher
-    pose_pub = rospy.Publisher("/robot_pose", PoseStamped, queue_size=10)
-
-    rate = rospy.Rate(10)  # 10 Hz
-    while not rospy.is_shutdown():
-        pose = get_end_effector_position(listener) 
-        pose_pub.publish(pose)
-        rospy.loginfo("Published End-effector Pose to /robot_pose: %s", pose)
-        rate.sleep()
+    robot_pose = RobotPose()
+    robot_pose.publish_pose()
