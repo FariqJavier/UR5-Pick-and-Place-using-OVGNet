3448f74b2b594b3e946c078f3f4b2f57028475f8
FariqJavier <fariqjavier@gmail.com>
2025-02-25 23:11:29 +0700

    publishing robot pose on joint state callback

--- workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_pose.py
+++ workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_pose.py
@@ -3,14 +3,15 @@ import rospy
 import tf
 from geometry_msgs.msg import PoseStamped
 from tf.transformations import quaternion_from_euler
+from sensor_msgs.msg import JointState
 class RobotPose:
     def __init__(self):
         rospy.init_node('custom_robot_pose', anonymous=True)
         self.listener = tf.TransformListener()
         self.pose_pub = rospy.Publisher("/robot_pose", PoseStamped, queue_size=10)
-        self.rate = rospy.Rate(10)
+        self.joint_sub = rospy.Subscriber("/joint_states", JointState, self.publish_pose_on_callback)
 
-    def get_end_effector_position(self):
+    def get_end_effector_pose(self):
         try:
             self.listener.waitForTransform('/base_link', '/flange', rospy.Time(0), rospy.Duration(4.0))
             self.listener.waitForTransform('/flange', '/robotiq_arg2f_base_link', rospy.Time(0), rospy.Duration(4.0))
@@ -38,16 +39,13 @@ class RobotPose:
             rospy.logerr("Transform not available!")
             return None
 
-    def publish_pose(self):
-        while not rospy.is_shutdown():
-            pose = self.get_end_effector_position()
-            if pose:
-                self.pose_pub.publish(pose)
-                rospy.loginfo("Published End-effector Pose to /robot_pose: %s", pose)
-            self.rate.sleep()
+    def publish_pose_on_callback(self, msg):
+        pose = self.get_end_effector_pose()
+        if pose:
+            self.pose_pub.publish(pose)
+            rospy.loginfo("Published End-effector Pose to /robot_pose: %s", pose)
 
 if __name__ == "__main__":
     print("RobotPose script started")
-
     robot_pose = RobotPose()
-    robot_pose.publish_pose()
+    rospy.spin()
