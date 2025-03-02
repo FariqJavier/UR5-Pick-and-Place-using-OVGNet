c87a2c4ece6bed8bfa0cfdd1fa0829828fce9759
FariqJavier <fariqjavier@gmail.com>
2025-03-02 23:57:29 +0700

    add gripper joint control

--- workspace/noetic_src/ur_pick_and_place/scripts/custom_moveit_manipulator.py
+++ workspace/noetic_src/ur_pick_and_place/scripts/custom_moveit_manipulator.py
@@ -6,8 +6,8 @@ from geometry_msgs.msg import Pose
 from pynput.keyboard import Listener, Key,KeyCode
 
 EF_POS_STEP = 0.05  # Position increment/decrement
-EF_ANGLE_POS = 0.005  # Orientation increment/decrement
-GRIPPER_JOINT_STEP = 0.005
+EF_ANGLE_POS = 0.05  # Orientation increment/decrement
+GRIPPER_JOINT_STEP = 0.1
 
 ESCAPE_COMBS = [
     {Key.ctrl_l, KeyCode(char='c')},
@@ -16,21 +16,23 @@ ESCAPE_COMBS = [
 
 
 CONTROL_MSG = """
-Control Your UR5 with MoveIt!
+Test Control Your UR5 with MoveIt!
 ---------------------------
-Moving end-effector around:
+Move Test end-effector around:
         W
    A    S    D
 
-Arm position step += 0.05
-Arm angle step += 0.005
+Arm position step +-= 0.05
+Arm angle step +-= 0.05
+Gripper Joint step +-= 0.1
 
-W: Move Upward
-A: Move Leftside
-S: Move Downward
-D: Move Rightside
+W: Move Arm Upward
+A: Move Arm Leftside
+S: Move Arm Downward
+D: Move Arm Rightside
+Left Shift: Close Gripper
+Right Shift: Right Gripper
 
-SPACE KEY : force stop
 
 CTRL-C to quit
 """
@@ -49,13 +51,13 @@ class MoveItManipulator:
         self.gripper_group = moveit_commander.MoveGroupCommander("gripper")
 
         self.target_ef_pose = None
-
         self.target_ef_traj = None
         
         self.target_gripper_joint = None
+        self.target_gripper_traj = None
 
         self.escape_key_pressed = None
-        # self.get_keys()
+        self.get_keys()
 
     def get_latest_ef_pose(self):
         return self.arm_group.get_current_pose()
@@ -65,6 +67,7 @@ class MoveItManipulator:
 
     def set_target_ef_pose_by_teleop(self, key):
         self.target_ef_pose = self.get_latest_ef_pose()
+        self.target_gripper_joint = self.get_latest_gripper_joint()
 
         if key in [Key.ctrl_l, Key.ctrl_r]:  
             self.escape_key_pressed.add(key)
@@ -90,6 +93,14 @@ class MoveItManipulator:
         elif key== Key.left:
             self.target_ef_pose.pose.orientation.z -= EF_POS_STEP
             self.plan_and_execute_pose()
+
+        if key == Key.shift_r:
+            self.target_gripper_joint[0] -= GRIPPER_JOINT_STEP
+            self.plan_and_execute_pose()
+        elif key == Key.shift_l:
+            self.target_gripper_joint[0] += GRIPPER_JOINT_STEP
+            self.plan_and_execute_pose()
+            
 
         try:
             if key.char == 'a':
@@ -124,18 +135,38 @@ class MoveItManipulator:
                 moveit_commander.roscpp_shutdown()
 
     def plan_and_execute_pose(self):
+        if not self.target_ef_pose:
+            rospy.logwarn("No arm target pose set. Skipping planning and execution.")
+            return
+        else:
+            self.arm_group.set_pose_target(self.target_ef_pose)
+            success, self.target_ef_traj, _, _ = self.arm_group.plan()
+            print(success)
+            self.arm_group.execute(self.target_ef_traj)
+            rospy.loginfo("Executed arm planned trajectory: %s", self.target_ef_traj)
+
+        if not self.target_gripper_joint:
+            rospy.logwarn("No arm gripper joint set. Skipping planning and execution.")
+            return
+        else:
+            self.gripper_group.set_joint_value_target(self.target_gripper_joint)
+            success, self.target_gripper_traj, _, _ = self.gripper_group.plan()
+            print(success)
+            self.gripper_group.execute(self.target_gripper_traj)
+            rospy.loginfo("Executed gripper planned trajectory: %s", self.target_gripper_traj)
+
         # if not self.target_ef_pose:
         #     rospy.logwarn("No target pose set. Skipping planning and execution.")
         #     return
 
         # self.arm_group.set_pose_target(self.target_ef_pose)
-        self.arm_group.set_named_target("arm_ready")
-        success, self.target_ef_traj, _, _ = self.arm_group.plan()
-        print(success)
-        self.arm_group.execute(self.target_ef_traj)
-        rospy.loginfo("Executed planned trajectory: %s", self.target_ef_traj)
+        # # self.arm_group.set_named_target("arm_ready")
+        # success, self.target_ef_traj, _, _ = self.arm_group.plan()
+        # print(success)
+        # self.arm_group.execute(self.target_ef_traj)
+        # rospy.loginfo("Executed planned trajectory: %s", self.target_ef_traj)
 
 if __name__ == "__main__":
     print("MoveItManipulator script started")
     moveit_manipulator = MoveItManipulator()
-    moveit_manipulator.plan_and_execute_pose()
+    # moveit_manipulator.plan_and_execute_pose()
--- workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_manipulator.py
+++ workspace/noetic_src/ur_pick_and_place/scripts/custom_robot_manipulator.py
@@ -11,6 +11,7 @@ from sensor_msgs.msg import JointState
 
 class RobotManipulator:
     def __init__(self):
+        moveit_commander.roscpp_initialize([])
         rospy.init_node('custom_robot_manipulator', anonymous=True)
         self.robot = moveit_commander.RobotCommander()
         self.arm_group = moveit_commander.MoveGroupCommander("arm")
@@ -18,6 +19,7 @@ class RobotManipulator:
 
         self.latest_arm_traj = None
         self.target_arm_traj = None
+        self.target_arm_pose = None
 
         self.arm_traj_pub = rospy.Publisher("/arm_trajectory", JointTrajectory, queue_size=10)
         
@@ -33,8 +35,6 @@ class RobotManipulator:
             "wrist_2_joint",
             "wrist_3_joint"
         ]
-
-        rospy.Rate(125)
         
     def arm_traj_callback(self, msg):
         if len(msg.position) != 6:
@@ -51,20 +51,20 @@ class RobotManipulator:
         latest_arm_pose = msg.pose
 
         # Set a new target position (e.g., move slightly upwards)
-        target_arm_pose = PoseStamped()
-        target_arm_pose.header.stamp = rospy.Time.now()
-        target_arm_pose.header.frame_id = ""
+        self.target_arm_pose = PoseStamped()
+        self.target_arm_pose.header.stamp = rospy.Time.now()
+        self.target_arm_pose.header.frame_id = ""
 
-        target_arm_pose.pose.position.x = latest_arm_pose.position.x
-        target_arm_pose.pose.position.y = latest_arm_pose.position.y
-        target_arm_pose.pose.position.z = latest_arm_pose.position.z + 0.05
-        target_arm_pose.pose.orientation = latest_arm_pose.orientation  # Keep the same orientation
+        self.target_arm_pose.pose.position.x = latest_arm_pose.position.x
+        self.target_arm_pose.pose.position.y = latest_arm_pose.position.y
+        self.target_arm_pose.pose.position.z = latest_arm_pose.position.z + 0.05
+        self.target_arm_pose.pose.orientation = latest_arm_pose.orientation  # Keep the same orientation
 
         # Compute inverse kinematics to get joint positions
         sol = self.arm_ik_solver.get_ik(
             self.latest_arm_traj,
-            target_arm_pose.pose.position.x, target_arm_pose.pose.position.y, target_arm_pose.pose.position.z,
-            target_arm_pose.pose.orientation.x, target_arm_pose.pose.orientation.y, target_arm_pose.pose.orientation.z, target_arm_pose.pose.orientation.w
+            self.target_arm_pose.pose.position.x, self.target_arm_pose.pose.position.y, self.target_arm_pose.pose.position.z,
+            self.target_arm_pose.pose.orientation.x, self.target_arm_pose.pose.orientation.y, self.target_arm_pose.pose.orientation.z, self.target_arm_pose.pose.orientation.w
         )
         
         if sol:
@@ -95,18 +95,24 @@ class RobotManipulator:
                 rospy.logwarn("No valid trajectory to publish")
             else:
                 traj_msg = self.create_arm_traj_msg()
-                self.arm_traj_pub.publish(traj_msg)
-                rospy.loginfo("Published joint trajectory: %s", traj_msg)
-            # rospy.sleep(1/125)
+                print(self.arm_group.get_current_pose())
+                print(self.arm_group.get_current_joint_values())
+                print(self.arm_group.get_current_rpy())
+                print(self.arm_group.get_current_state())
+                # self.arm_traj_pub.publish(traj_msg)
+                # rospy.loginfo("Published joint trajectory: %s", traj_msg)
+                self.arm_group.set_joint_value_target(self.target_arm_traj)
+                success, plan, _, _ = self.arm_group.plan()
+                if success: 
+                    self.arm_group.execute(plan, wait=True)
+                    rospy.loginfo("Executed trajectory using MoveIt!")
+                else:
+                    rospy.logwarn("Failed to generate a valid plan.")
+
             rospy.sleep(1) # Publish every 1 second
+        moveit_commander.roscpp_shutdown()
 
 if __name__ == '__main__':
-    moveit_commander.roscpp_initialize([])
-    try:
-        print("RobotManipulator script started")
-        robot_manipulator = RobotManipulator()
-        robot_manipulator.publish_arm_traj()
-    except rospy.ROSInterruptException:
-        pass
-    finally:
-        moveit_commander.roscpp_shutdown()
+    print("RobotManipulator script started")
+    robot_manipulator = RobotManipulator()
+    robot_manipulator.publish_arm_traj()
