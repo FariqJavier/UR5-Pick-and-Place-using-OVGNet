ce585e6980c97a947f9bf6262561e41cdfbf503f
FariqJavier <fariqjavier@gmail.com>
2025-05-29 23:56:25 +0700

    test offset on pose yaw orientation

--- workspace/noetic_src/ur_pick_and_place/scripts/custom_pick_and_place_motion.py
+++ workspace/noetic_src/ur_pick_and_place/scripts/custom_pick_and_place_motion.py
@@ -5,6 +5,7 @@ import os
 import sys
 import numpy as np
 from std_msgs.msg import Bool
+import math
 from geometry_msgs.msg import PoseStamped
 from moveit_msgs.msg import Constraints, OrientationConstraint
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
@@ -29,7 +30,7 @@ class CustomPickAndPlaceMotion:
             rospy.loginfo('Best Grasp Pose Received: %s', msg)
             rospy.loginfo("Executing Picking Motion ...")
 
-            # self.run_picking_pose(msg)
+            self.run_picking_pose(msg)
             # self.run_ready_pose()
             
             self.completed_motion_pub.publish(Bool(data=False))
@@ -66,71 +67,23 @@ class CustomPickAndPlaceMotion:
             # self.arm_group.set_path_constraints(constraints)
 
             # 3. Set target pose dan plan
-            yaw_pose_stamped = PoseStamped()
-            yaw_pose_stamped.header.stamp = rospy.Time.now()
-            yaw_pose_stamped.header.frame_id = self.reference_frame
-            # Set the current position (keep the same position as the current pose)
-            yaw_pose_stamped.pose.position = self.arm_group.get_current_pose().pose.position
 
-            # Apply a 90-degree yaw rotation (90 degrees = π/2 radians)
-            yaw_angle = 90  # degrees
-            yaw_radians = yaw_angle * (3.14159 / 180)  # Convert to radians
-
-            # Convert the yaw rotation to a quaternion (roll=0, pitch=0, yaw=90 degrees)
-            quaternion = quaternion_from_euler(0, 0, yaw_radians)
-
-            # Set the orientation (quaternion) of the pose
-            yaw_pose_stamped.pose.orientation.x = quaternion[0]
-            yaw_pose_stamped.pose.orientation.y = quaternion[1]
-            yaw_pose_stamped.pose.orientation.z = quaternion[2]
-            yaw_pose_stamped.pose.orientation.w = quaternion[3]
-
-            # Log the pose
-            rospy.loginfo(f'Executing Pose (Orientation: Yaw 90 degrees): {yaw_pose_stamped}')
-            # Set the target pose for the robot
-            self.arm_group.set_pose_target(yaw_pose_stamped)
-            # Plan and execute the movement
-            success, traj_plan, _, _ = self.arm_group.plan()
-            self.arm_group.stop()
-            self.arm_group.clear_pose_targets()
-            if not success:
-                rospy.logerr("Failed to plan movement to 90-degree yaw pose.")
-                return
-            self.arm_group.execute(traj_plan, wait=True)
-            rospy.loginfo("Executed planned trajectory to 90-degree yaw pose ...")
-
-            # Test gerakan translasi ke posisi picking
-            pose_stamped = PoseStamped()
-            pose_stamped.header.stamp = rospy.Time.now()
-            pose_stamped.header.frame_id = self.reference_frame
-            pose_stamped.pose.position.x = pose_msg.pose.position.x
-            pose_stamped.pose.position.y = pose_msg.pose.position.y
-            pose_stamped.pose.position.z = pose_msg.pose.position.z
-            pose_stamped.pose.orientation = self.arm_group.get_current_pose().pose.orientation
-
-            rospy.loginfo(f'Executing Pose (Translation): {pose_stamped}')
-
-            self.arm_group.set_pose_target(pose_stamped)
-            success, traj_plan, _, _ = self.arm_group.plan()
-
-            self.arm_group.stop()
-            self.arm_group.clear_pose_targets()
-            # self.arm_group.clear_path_constraints()  # 4. Hapus constraint setelah plan
-
-            if not success:
-                rospy.logerr("Failed to plan movement to picking pose (Translation).")
-                return
-
-            self.arm_group.execute(traj_plan, wait=True)
-            rospy.loginfo("Executed planned trajectory to picking pose (Translation) ...")
-
-            # # Test gerakan orientasi ke posisi picking
-            # # Convert the target quaternion (orientation_target) to roll, pitch, yaw
-            # roll, pitch, yaw = euler_from_quaternion([pose_msg.pose.orientation.x, 
-            #                                         pose_msg.pose.orientation.y, 
-            #                                         pose_msg.pose.orientation.z, 
-            #                                         pose_msg.pose.orientation.w])
-            # # Roll Motion
+            # Test gerakan orientasi ke posisi picking
+            # Convert the target quaternion (orientation_target) to roll, pitch, yaw
+            target_roll, target_pitch, target_yaw = euler_from_quaternion([
+                pose_msg.pose.orientation.x, 
+                pose_msg.pose.orientation.y, 
+                pose_msg.pose.orientation.z, 
+                pose_msg.pose.orientation.w
+            ])
+            target_yaw -= math.radians(33)  # Tambahkan rotasi 180 derajat pada yaw
+            current_roll, current_pitch, current_yaw = euler_from_quaternion([
+                self.arm_group.get_current_pose().pose.orientation.x, 
+                self.arm_group.get_current_pose().pose.orientation.y, 
+                self.arm_group.get_current_pose().pose.orientation.z, 
+                self.arm_group.get_current_pose().pose.orientation.w
+            ])
+            # Roll Motion
             # roll_pose_stamped = PoseStamped()
             # roll_pose_stamped.header.stamp = rospy.Time.now()
             # roll_pose_stamped.header.frame_id = self.reference_frame
@@ -176,40 +129,65 @@ class CustomPickAndPlaceMotion:
             # self.arm_group.execute(traj_plan, wait=True)
             # rospy.loginfo("Executed planned trajectory to picking pose (Orientation: Pitch) ...")
 
-            # # Yaw Motion
-            # yaw_pose_stamped = PoseStamped()
-            # yaw_pose_stamped.header.stamp = rospy.Time.now()
-            # yaw_pose_stamped.header.frame_id = self.reference_frame
-            # yaw_pose_stamped.pose.position = self.arm_group.get_current_pose().pose.position
-            # # Convert yaw to quaternion and set it
-            # yaw_quat = quaternion_from_euler(0, 0, yaw)
-            # yaw_pose_stamped.pose.orientation.x = yaw_quat[0]
-            # yaw_pose_stamped.pose.orientation.y = yaw_quat[1]
-            # yaw_pose_stamped.pose.orientation.z = yaw_quat[2]
-            # yaw_pose_stamped.pose.orientation.w = yaw_quat[3]
-            # rospy.loginfo(f'Executing Pose (Orientation: Yaw): {yaw_pose_stamped}')
-            # self.arm_group.set_pose_target(yaw_pose_stamped)
-            # success, traj_plan, _, _ = self.arm_group.plan()
-            # self.arm_group.stop()
-            # self.arm_group.clear_pose_targets()
-            # # self.arm_group.clear_path_constraints()  # 4. Hapus constraint setelah plan
-            # if not success:
-            #     rospy.logerr("Failed to plan movement to picking pose (Orientation: Yaw).")
-            #     return
-            # self.arm_group.execute(traj_plan, wait=True)
-            # rospy.loginfo("Executed planned trajectory to picking pose (Orientation: Yaw) ...")
-
-            self.gripper_group.set_named_target("gripper_big_close")
+            # Yaw Motion
+            yaw_pose_stamped = PoseStamped()
+            yaw_pose_stamped.header.stamp = rospy.Time.now()
+            yaw_pose_stamped.header.frame_id = self.reference_frame
+            yaw_pose_stamped.pose.position = self.arm_group.get_current_pose().pose.position
+            # Convert yaw to quaternion and set it
+            yaw_quat = quaternion_from_euler(current_roll, current_pitch, target_yaw)
+            yaw_pose_stamped.pose.orientation.x = yaw_quat[0]
+            yaw_pose_stamped.pose.orientation.y = yaw_quat[1]
+            yaw_pose_stamped.pose.orientation.z = yaw_quat[2]
+            yaw_pose_stamped.pose.orientation.w = yaw_quat[3]
+            rospy.loginfo(f'Executing Pose (Orientation: Yaw): {yaw_pose_stamped}')
+            self.arm_group.set_pose_target(yaw_pose_stamped)
             success, traj_plan, _, _ = self.arm_group.plan()
-            self.gripper_group.stop() # Ensures no residual movement
-            self.gripper_group.clear_pose_targets()
+            self.arm_group.stop()
+            self.arm_group.clear_pose_targets()
+            # self.arm_group.clear_path_constraints()  # 4. Hapus constraint setelah plan
+            if not success:
+                rospy.logerr("Failed to plan movement to picking pose (Orientation: Yaw).")
+                return
+            self.arm_group.execute(traj_plan, wait=True)
+            rospy.loginfo("Executed planned trajectory to picking pose (Orientation: Yaw) ...")
+
+            # Gerakan translasi ke posisi picking
+            pose_stamped = PoseStamped()
+            pose_stamped.header.stamp = rospy.Time.now()
+            pose_stamped.header.frame_id = self.reference_frame
+            pose_stamped.pose.position.x = pose_msg.pose.position.x
+            pose_stamped.pose.position.y = pose_msg.pose.position.y
+            pose_stamped.pose.position.z = pose_msg.pose.position.z
+            pose_stamped.pose.orientation = self.arm_group.get_current_pose().pose.orientation
+
+            rospy.loginfo(f'Executing Pose (Translation): {pose_stamped}')
+
+            self.arm_group.set_pose_target(pose_stamped)
+            success, traj_plan, _, _ = self.arm_group.plan()
+
+            self.arm_group.stop()
+            self.arm_group.clear_pose_targets()
+            # self.arm_group.clear_path_constraints()  # 4. Hapus constraint setelah plan
 
             if not success:
-                rospy.logerr("Failed to plan movement closing gripper.")
+                rospy.logerr("Failed to plan movement to picking pose (Translation).")
                 return
 
-            self.gripper_group.execute(traj_plan, wait=True)
-            rospy.loginfo("Executed planned trajectory to Big Closing Gripper ...")
+            self.arm_group.execute(traj_plan, wait=True)
+            rospy.loginfo("Executed planned trajectory to picking pose (Translation) ...")
+
+            # self.gripper_group.set_named_target("gripper_big_close")
+            # success, traj_plan, _, _ = self.arm_group.plan()
+            # self.gripper_group.stop() # Ensures no residual movement
+            # self.gripper_group.clear_pose_targets()
+
+            # if not success:
+            #     rospy.logerr("Failed to plan movement closing gripper.")
+            #     return
+
+            # self.gripper_group.execute(traj_plan, wait=True)
+            # rospy.loginfo("Executed planned trajectory to Big Closing Gripper ...")
 
         except Exception as e:
             rospy.logerr("An error occurred while moving to picking pose: %s", str(e))
